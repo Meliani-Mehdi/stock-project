e85f4debe0704b39b49f8268221ec5a1ca9538d6
Meliani Mehdi <138245500+Meliani-Mehdi@users.noreply.github.com>
2023-12-31 20:51:40 +0100

    Update LoginController.java

--- Gl_Project/src/main/java/Org/Main/Controllers/LoginController.java
+++ Gl_Project/src/main/java/Org/Main/Controllers/LoginController.java
@@ -6,14 +6,40 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.util.Duration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class LoginController {
+
+    public static void createDatabase() {
+        String url = "jdbc:sqlite:your-database-name.db";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement statement = conn.createStatement()) {
+
+            // Create a sample table
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS users (\n"
+                    + "	id integer PRIMARY KEY,\n"
+                    + "	username text NOT NULL,\n"
+                    + "	password text NOT NULL\n"
+                    + ");";
+
+            statement.execute(createTableSQL);
+
+            System.out.println("Database and table created successfully.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     @FXML
     private Button test;
     public void Test() {
-        ScaleTransition scale=new ScaleTransition(Duration.millis(500),test);
-        scale.setByY(1.5);
-        scale.setByX(1.5);
-        scale.setCycleCount(1);
-        scale.play();
+        createDatabase();
+        System.out.println("noice");
     }
 }
