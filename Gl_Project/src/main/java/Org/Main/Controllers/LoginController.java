0cdd14a26265a338ee05697ea352bfca1adbb61c
zamouni <124431626+zamouni@users.noreply.github.com>
2024-01-02 17:08:09 +0100

    the login page

--- Gl_Project/src/main/java/Org/Main/Controllers/LoginController.java
+++ Gl_Project/src/main/java/Org/Main/Controllers/LoginController.java
@@ -1,13 +1,51 @@
 package Org.Main.Controllers;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
+    @FXML
+    private AnchorPane Main_Pane;
+    private Stage stage;
+
+    @FXML
+    private TextField SignIn_Username_Textfield;
+
+    @FXML
+    private PasswordField SignIn_Password_TextField;
+    public void Exit(){
+        stage = (Stage) Main_Pane.getScene().getWindow();
+        System.exit(0);
+    }
+    public void Minimize(){
+        stage = (Stage) Main_Pane.getScene().getWindow();
+
+        stage.setIconified(true);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Main_Pane.requestFocus();
+
+        Main_Pane.addEventFilter(javafx.scene.input.KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                Sign_in_Button.fire();
+            }
+        });
+    }
 
     public static void createDatabase() {
         String url = "jdbc:sqlite:your-database-name.db";
@@ -30,12 +68,13 @@ public class LoginController {
             e.printStackTrace();
         }
     }
-
-
     @FXML
-    private Button test;
+    private Button Sign_in_Button;
     public void Test() {
         createDatabase();
         System.out.println("noice");
     }
+
+    @FXML
+    private Label Need_An_Account_Button;
 }
--- Gl_Project/src/main/java/Org/Main/Main.java
+++ Gl_Project/src/main/java/Org/Main/Main.java
@@ -4,16 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
 public class Main extends Application {
+    private double xOffset, yOffset;
     @Override
     public void start(Stage stage) throws Exception {
+        Font.loadFont(getClass().getResourceAsStream("/Fonts/Quicksand-VariableFont_wght.ttf"), 12);
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/Login.fxml")));
         Scene scene=new Scene(root);
+        root.setOnMousePressed(e -> {
+            xOffset = e.getSceneX();
+            yOffset = e.getSceneY();
+        });
+        root.setOnMouseDragged(e -> {
+            stage.setX(e.getScreenX() - xOffset);
+            stage.setY(e.getScreenY() - yOffset);
+        });
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.getScene().getRoot().setStyle("-fx-background-radius: 10;");
         stage.show();
     }
 
