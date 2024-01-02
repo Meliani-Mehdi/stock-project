27c0b63a43e2a6a62ea11e97df7b856e6685830e
Meliani Mehdi <Meliani-Mehdi@users.noreply.github.com>
2024-01-03 00:56:05 +0100

    user testing complete!

--- Gl_Project/src/main/java/Org/Main/Controllers/LoginController.java
+++ Gl_Project/src/main/java/Org/Main/Controllers/LoginController.java
@@ -10,10 +10,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -42,151 +39,24 @@ public class LoginController implements Initializable {
         });
     }
 
-    public static void createDatabase() {
-        String url = "jdbc:sqlite:your-database-name.db";
+
+    static public void searchLog(String name, String pass){
+        String url = "jdbc:sqlite:main.db";
         try (Connection conn = DriverManager.getConnection(url);
-             Statement statement = conn.createStatement()) {
+             PreparedStatement quary = conn.prepareStatement("""
+                            SELECT * FROM users WHERE username = ? AND password = ? ;
+                            """)) {
 
-            // Create clients table
-            String createClientsTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS clients (
-                                id INTEGER PRIMARY KEY,
-                                name TEXT NOT NULL,
-                                adresse TEXT,
-                                phone_num TEXT,
-                                sold_total REAL,
-                                reste REAL,
-                                paid REAL
-                            );""";
-            statement.execute(createClientsTableSQL);
-            System.out.println("Table 'clients' created successfully.");
+            quary.setString(1,name);
+            quary.setString(2,pass);
 
-            // Create deposits table
-            String createDepositsTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS deposits (
-                                id INTEGER PRIMARY KEY,
-                                name TEXT NOT NULL,
-                                solde REAL,
-                                reste REAL,
-                                payment REAL,
-                                id_provider INTEGER,
-                                id_client INTEGER,
-                                id_seller INTEGER,
-                                type TEXT,
-                                payment_status TEXT
-                            );""";
-            statement.execute(createDepositsTableSQL);
-            System.out.println("Table 'deposits' created successfully.");
-
-            // Create factures table
-            String createFacturesTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS factures (
-                                id INTEGER PRIMARY KEY,
-                                date DATE NOT NULL,
-                                update_date DATE,
-                                update_time TIMESTAMP,
-                                solde REAL,
-                                reste REAL,
-                                payment REAL,
-                                id_provider INTEGER,
-                                id_client INTEGER,
-                                id_seller INTEGER,
-                                type TEXT,
-                                payment_status TEXT
-                            );""";
-            statement.execute(createFacturesTableSQL);
-            System.out.println("Table 'factures' created successfully.");
-
-            // Create groupes table
-            String createGroupesTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS groupes (
-                                id INTEGER PRIMARY KEY,
-                                name TEXT NOT NULL,
-                                marge REAL
-                            );""";
-            statement.execute(createGroupesTableSQL);
-            System.out.println("Table 'groupes' created successfully.");
-
-            // Create products table
-            String createProductsTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS products (
-                                id INTEGER PRIMARY KEY,
-                                bar_code VARCHAR NOT NULL,
-                                reference VARCHAR,
-                                name VARCHAR NOT NULL,
-                                buying_price REAL,
-                                selling_price REAL,
-                                stock INTEGER,
-                                photo VARCHAR,
-                                id_groupe INTEGER
-                            );""";
-            statement.execute(createProductsTableSQL);
-            System.out.println("Table 'products' created successfully.");
-
-            // Create product_facts table
-            String createProductFactsTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS product_facts (
-                                id INTEGER PRIMARY KEY,
-                                id_fact INTEGER,
-                                id_prod INTEGER,
-                                product_price REAL,
-                                product_qte INTEGER,
-                                tva REAL
-                            );""";
-            statement.execute(createProductFactsTableSQL);
-            System.out.println("Table 'product_facts' created successfully.");
-
-            // Create bon_de_livraisons table
-            String createBonDeLivraisonsTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS bon_de_livraisons (
-                                id INTEGER PRIMARY KEY,
-                                date DATE NOT NULL,
-                                old_reste REAL,
-                                reste REAL,
-                                payment REAL,
-                                id_client INTEGER,
-                                id_user INTEGER,
-                                id_provider INTEGER
-                            );""";
-            statement.execute(createBonDeLivraisonsTableSQL);
-            System.out.println("Table 'bon_de_livraisons' created successfully.");
-
-            // Create providers table
-            String createProvidersTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS providers (
-                                id INTEGER PRIMARY KEY,
-                                name TEXT NOT NULL,
-                                adresse TEXT,
-                                phone_num TEXT,
-                                sold_total REAL,
-                                reste REAL,
-                                paid REAL
-                            );""";
-            statement.execute(createProvidersTableSQL);
-            System.out.println("Table 'providers' created successfully.");
-
-            // Create users table
-            String createUsersTableSQL =
-                    """
-                            CREATE TABLE IF NOT EXISTS users (
-                                id INTEGER PRIMARY KEY,
-                                username TEXT NOT NULL,
-                                password TEXT NOT NULL,
-                                'last' INTEGER NOT NULL
-                            );""";
-            statement.execute(createUsersTableSQL);
-
-            System.out.println("Table 'users' created successfully.");
-
-            System.out.println("Database and tables created successfully.");
+            try (ResultSet resultSet = quary.executeQuery()) {
+                if (resultSet.next()) {
+                    System.out.println("\nWelcome "+name+"!");
+                } else {
+                    System.out.println("\nUser not found, please check your info.");
+                }
+            }
 
         } catch (SQLException e) {
             System.out.println("error");
@@ -196,8 +66,7 @@ public class LoginController implements Initializable {
     @FXML
     private Button Sign_in_Button;
     public void Test() {
-        createDatabase();
-        System.out.println("noice");
+        searchLog(SignIn_Username_Textfield.getText(),SignIn_Password_TextField.getText());
     }
 
     @FXML
@@ -205,6 +74,15 @@ public class LoginController implements Initializable {
 
     @FXML
     private PasswordField SignIn_Password_TextField;
-// create a user in the database and try to log in with that user if the user info is not wrong the user will log in if the user info are wrong print wrong
-    // close the database
+
+    //users that I made for testing :
+    /*
+    mehdi,prrr
+    bob,12345
+    3dnan,mathIsGood
+    kazuha,imOpAF
+    test,test
+    admin,admin
+     */
+
 }
--- Gl_Project/src/main/java/Org/Main/Main.java
+++ Gl_Project/src/main/java/Org/Main/Main.java
@@ -8,6 +8,10 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Objects;
 
 public class Main extends Application {
@@ -17,6 +21,7 @@ public class Main extends Application {
         Font.loadFont(getClass().getResourceAsStream("/Fonts/Quicksand-VariableFont_wght.ttf"), 12);
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/Login.fxml")));
         Scene scene=new Scene(root);
+        createDatabase();
         root.setOnMousePressed(e -> {
             xOffset = e.getSceneX();
             yOffset = e.getSceneY();
@@ -31,6 +36,123 @@ public class Main extends Application {
         stage.initStyle(StageStyle.UNDECORATED);
         stage.getScene().getRoot().setStyle("-fx-background-radius: 10;");
         stage.show();
+    }
+
+    public static void createDatabase() {
+        String url = "jdbc:sqlite:main.db";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement statement = conn.createStatement()) {
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS clients (
+                                id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                adresse TEXT,
+                                phone_num TEXT,
+                                sold_total REAL,
+                                reste REAL,
+                                paid REAL
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS deposits (
+                                id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                solde REAL,
+                                reste REAL,
+                                payment REAL,
+                                id_provider INTEGER,
+                                id_client INTEGER,
+                                id_seller INTEGER,
+                                type TEXT,
+                                payment_status TEXT
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS factures (
+                                id INTEGER PRIMARY KEY,
+                                date DATE NOT NULL,
+                                update_date DATE,
+                                update_time TIMESTAMP,
+                                solde REAL,
+                                reste REAL,
+                                payment REAL,
+                                id_provider INTEGER,
+                                id_client INTEGER,
+                                id_seller INTEGER,
+                                type TEXT,
+                                payment_status TEXT
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS groupes (
+                                id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                marge REAL
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS products (
+                                id INTEGER PRIMARY KEY,
+                                bar_code VARCHAR NOT NULL,
+                                reference VARCHAR,
+                                name VARCHAR NOT NULL,
+                                buying_price REAL,
+                                selling_price REAL,
+                                stock INTEGER,
+                                photo VARCHAR,
+                                id_groupe INTEGER
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS product_facts (
+                                id INTEGER PRIMARY KEY,
+                                id_fact INTEGER,
+                                id_prod INTEGER,
+                                product_price REAL,
+                                product_qte INTEGER,
+                                tva REAL
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS bon_de_livraisons (
+                                id INTEGER PRIMARY KEY,
+                                date DATE NOT NULL,
+                                old_reste REAL,
+                                reste REAL,
+                                payment REAL,
+                                id_client INTEGER,
+                                id_user INTEGER,
+                                id_provider INTEGER
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS providers (
+                                id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                adresse TEXT,
+                                phone_num TEXT,
+                                sold_total REAL,
+                                reste REAL,
+                                paid REAL
+                            );""");
+
+            statement.execute("""
+                            CREATE TABLE IF NOT EXISTS users (
+                                id INTEGER PRIMARY KEY,
+                                username TEXT NOT NULL,
+                                password TEXT NOT NULL,
+                                'last' INTEGER NOT NULL
+                            );""");
+
+            System.out.println("Database and tables created successfully.");
+            conn.close();
+            System.out.println(conn.isClosed());
+
+
+        } catch (SQLException e) {
+            System.out.println("error");
+        }
     }
 
     public static void main(String[] args) {
